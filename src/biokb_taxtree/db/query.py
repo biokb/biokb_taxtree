2eece93fe6e06c051d5572de1b46658b733e38ef
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2025-05-08 13:46:32 +0200

    rearrangement of modules

--- src/biokb_taxtree/db/query.py
+++ src/biokb_taxtree/db/query.py
@@ -1,12 +1,17 @@
-from typing import Optional
+import logging
 
 from sqlalchemy import Engine
 from sqlalchemy.orm import sessionmaker
 
 from biokb_taxtree.db import models
+from biokb_taxtree.logger import setup_logging
+
+setup_logging()
+
+logger = logging.getLogger("query")
 
 
-class Query:
+class DbQuery:
 
     def __init__(self, engine: Engine) -> None:
         self.Session = sessionmaker(engine)
