05ba7cbebe2815ab7533ba4f47c86bf58015447a
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2025-12-05 19:14:09 +0100

    brenda namespace replaced by NCBI Taxtree NS

--- src/biokb_taxtree/rdf/namespaces.py
+++ src/biokb_taxtree/rdf/namespaces.py
@@ -1,27 +1,11 @@
 """RDF namespace URIs."""
 
 from rdflib import Namespace
-from biokb_taxtree.constants.brenda import BASE_URI
 
-tax_ns = Namespace("https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=")
+BASE_URI = "http://biokb.fraunhofer.de/taxtree/"
+
+NCBI_TAXON_NS = Namespace("http://purl.obolibrary.org/obo/NCBITaxon_")
 
 # BRENDA URIs to Fraunhofer
-node = Namespace(f"{BASE_URI}/node#")
-activation_ns = Namespace(f"{BASE_URI}/activation#")
-inhibition_ns = Namespace(f"{BASE_URI}/inhibition#")
-cofactor_ns = Namespace(f"{BASE_URI}/cofactor_interaction#")
-ic50_ns = Namespace(f"{BASE_URI}/ic50#")
-kcat_km_ns = Namespace(f"{BASE_URI}/kcat_km#")
-ki_ns = Namespace(f"{BASE_URI}/ki#")
-km_ns = Namespace(f"{BASE_URI}/km#")
-relation = Namespace(f"{BASE_URI}/relation#")
-location_ns = Namespace(f"{BASE_URI}/location#")
-metal_ion_ns = Namespace(f"{BASE_URI}/metal_ion#")
-nsp_reaction_ns = Namespace(f"{BASE_URI}/nsp_reaction#")
-sp_reaction_ns = Namespace(f"{BASE_URI}/sp_reaction#")
-information_ns = Namespace(f"{BASE_URI}/information#")
-
-# BRENDA URIs to BRENDA
-ec_ns = Namespace("https://www.brenda-enzymes.org/enzyme.php?ecno=")
-reaction_ns = Namespace("https://www.brenda-enzymes.org/enzyme.php?reaction=")
-compound_ns = Namespace("https://www.brenda-enzymes.org/ligand.php?brenda_ligand_id=")
+NODE_NS = Namespace(f"{BASE_URI}node#")
+RELATION_NS = Namespace(f"{BASE_URI}relation#")
