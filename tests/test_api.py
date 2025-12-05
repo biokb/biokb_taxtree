6953ec0922cbf156e325355394eb197443a5bcab
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2025-12-05 19:18:07 +0100

    changed, but have to adapted to the last changes

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,3 @@
-
 import os
 
 import pytest
@@ -7,8 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from biokb_taxtree.api.main import app, get_db
-from biokb_taxtree.db.manager import DbManager
 from biokb_taxtree.db.importer import DbImporter
+from biokb_taxtree.db.manager import DbManager
 
 # Create a new test database engine (SQLite in-memory for testing)
 os.makedirs("tests/dbs", exist_ok=True)
@@ -44,12 +43,16 @@ def client_with_data():
     dm = DbManager(test_engine)
     # Work around to setting the test data folder - could maybe implement this directly into the dbmanager
     di = DbImporter(engine=test_engine)
-    zipped_file_path= di.set_path_zip_file(os.path.join(test_data_folder,"dummy_taxtree_dump.zip"))
-    di._path_zip_file = os.path.join(test_data_folder,"dummy_taxtree_dump.zip")
-    unzipped_data_folder_path = di.set_path_unziped_data_folder(os.path.join(test_data_folder,"unzipped"))
+    zipped_file_path = di._set_path_zip_file(
+        os.path.join(test_data_folder, "dummy_taxtree_dump.zip")
+    )
+    di._path_zip_file = os.path.join(test_data_folder, "dummy_taxtree_dump.zip")
+    unzipped_data_folder_path = di._set_path_unziped_data_folder(
+        os.path.join(test_data_folder, "unzipped")
+    )
     dm.set_importer(di)
     dm.import_data()
-    return TestClient(app)  
+    return TestClient(app)
 
 
 def test_server(client_with_data: TestClient):
@@ -57,23 +60,24 @@ def test_server(client_with_data: TestClient):
     assert response.status_code == 200
     assert response.json() == {"msg": "Running!"}
 
+
 class TestName:
     def test_get_name(self, client_with_data: TestClient):
         response = client_with_data.get("/names/search/?name_txt=ancestor")
         assert response.status_code == 200
         data = response.json()
         expected = {
-        "count": 1,
-        "offset": 0,
-        "limit": 10,
-        "results": [
-            {
-            "name_txt": "ancestor",
-            "unique_name": None,
-            "name_class": "synonym",
-            "tax_id": 1
-            }
-        ]
+            "count": 1,
+            "offset": 0,
+            "limit": 10,
+            "results": [
+                {
+                    "name_txt": "ancestor",
+                    "unique_name": None,
+                    "name_class": "synonym",
+                    "tax_id": 1,
+                }
+            ],
         }
         assert data == expected
 
@@ -100,19 +104,20 @@ class TestName:
         assert response.status_code == 200
         data = response.json()
         expected = {
-        "count": 3,
-        "offset": 2,
-        "limit": 2,
-        "results": [
-            {
-            "name_txt": "child",
-            "unique_name": "Child <child>",
-            "name_class": "scientific name",
-            "tax_id": 3
-            }
-        ]
+            "count": 3,
+            "offset": 2,
+            "limit": 2,
+            "results": [
+                {
+                    "name_txt": "child",
+                    "unique_name": "Child <child>",
+                    "name_class": "scientific name",
+                    "tax_id": 3,
+                }
+            ],
         }
         assert data == expected
+
 
 class TestNode:
     def test_get_node(self, client_with_data: TestClient):
@@ -120,55 +125,55 @@ class TestNode:
         assert response.status_code == 200
         data = response.json()
         expected = {
-        "count": 1,
-        "offset": 0,
-        "limit": 10,
-        "results": [
-            {
-            "tax_id": 1,
-            "parent_tax_id": 1,
-            "rank": "no rank",
-            "embl_code": None,
-            "division_id": 8,
-            "inherited_div_flag": False,
-            "genetic_code_id": 1,
-            "inherited_gc_flag": False,
-            "mitochondrial_genetic_code_id": 0,
-            "inherited_mgc_flag": False,
-            "genbank_hidden_flag": False,
-            "hidden_subtree_root_flag": False,
-            "comments": None,
-            "plastid_genetic_code_id": None,
-            "inherited_pgc_flag": None,
-            "specified_species": False,
-            "hydrogenosome_genetic_code_id": 0,
-            "inherited_hgc_flag": True,
-            "tree_id": 1,
-            "tree_parent_id": None,
-            "level": 1,
-            "right_tree_id": 4,
-            "is_leaf": False,
-            "names": [
+            "count": 1,
+            "offset": 0,
+            "limit": 10,
+            "results": [
                 {
-                "name_txt": "ancestor",
-                "unique_name": None,
-                "name_class": "synonym"
+                    "tax_id": 1,
+                    "parent_tax_id": 1,
+                    "rank": "no rank",
+                    "embl_code": None,
+                    "division_id": 8,
+                    "inherited_div_flag": False,
+                    "genetic_code_id": 1,
+                    "inherited_gc_flag": False,
+                    "mitochondrial_genetic_code_id": 0,
+                    "inherited_mgc_flag": False,
+                    "genbank_hidden_flag": False,
+                    "hidden_subtree_root_flag": False,
+                    "comments": None,
+                    "plastid_genetic_code_id": None,
+                    "inherited_pgc_flag": None,
+                    "specified_species": False,
+                    "hydrogenosome_genetic_code_id": 0,
+                    "inherited_hgc_flag": True,
+                    "tree_id": 1,
+                    "tree_parent_id": None,
+                    "level": 1,
+                    "right_tree_id": 4,
+                    "is_leaf": False,
+                    "names": [
+                        {
+                            "name_txt": "ancestor",
+                            "unique_name": None,
+                            "name_class": "synonym",
+                        }
+                    ],
+                    "ranked_lineage": {
+                        "tax_id": 1,
+                        "tax_name": "root",
+                        "species": None,
+                        "genus": None,
+                        "family": None,
+                        "order": None,
+                        "class_": None,
+                        "phylum": None,
+                        "kingdom": None,
+                        "domain": "",
+                    },
                 }
             ],
-            "ranked_lineage": {
-                "tax_id": 1,
-                "tax_name": "root",
-                "species": None,
-                "genus": None,
-                "family": None,
-                "order": None,
-                "class_": None,
-                "phylum": None,
-                "kingdom": None,
-                "domain": ""
-            }
-            }
-        ]
         }
         assert data == expected
 
@@ -177,7 +182,7 @@ class TestNode:
         assert response.status_code == 200
         data = response.json()
         assert len(data["results"]) == 3
-    
+
     def test_list_nodes_offset(self, client_with_data: TestClient):
         response = client_with_data.get("/node/search/?offset=2")
         assert response.status_code == 200
@@ -195,57 +200,58 @@ class TestNode:
         assert response.status_code == 200
         data = response.json()
         expected = {
-        "count": 3,
-        "offset": 2,
-        "limit": 2,
-        "results": [
-            {
-            "tax_id": 3,
-            "parent_tax_id": 2,
-            "rank": "genus",
-            "embl_code": None,
-            "division_id": 0,
-            "inherited_div_flag": True,
-            "genetic_code_id": 11,
-            "inherited_gc_flag": True,
-            "mitochondrial_genetic_code_id": 0,
-            "inherited_mgc_flag": True,
-            "genbank_hidden_flag": False,
-            "hidden_subtree_root_flag": False,
-            "comments": "code compliant",
-            "plastid_genetic_code_id": None,
-            "inherited_pgc_flag": None,
-            "specified_species": False,
-            "hydrogenosome_genetic_code_id": 0,
-            "inherited_hgc_flag": True,
-            "tree_id": 3,
-            "tree_parent_id": 2,
-            "level": 2,
-            "right_tree_id": None,
-            "is_leaf": True,
-            "names": [
+            "count": 3,
+            "offset": 2,
+            "limit": 2,
+            "results": [
                 {
-                "name_txt": "child",
-                "unique_name": "Child <child>",
-                "name_class": "scientific name"
+                    "tax_id": 3,
+                    "parent_tax_id": 2,
+                    "rank": "genus",
+                    "embl_code": None,
+                    "division_id": 0,
+                    "inherited_div_flag": True,
+                    "genetic_code_id": 11,
+                    "inherited_gc_flag": True,
+                    "mitochondrial_genetic_code_id": 0,
+                    "inherited_mgc_flag": True,
+                    "genbank_hidden_flag": False,
+                    "hidden_subtree_root_flag": False,
+                    "comments": "code compliant",
+                    "plastid_genetic_code_id": None,
+                    "inherited_pgc_flag": None,
+                    "specified_species": False,
+                    "hydrogenosome_genetic_code_id": 0,
+                    "inherited_hgc_flag": True,
+                    "tree_id": 3,
+                    "tree_parent_id": 2,
+                    "level": 2,
+                    "right_tree_id": None,
+                    "is_leaf": True,
+                    "names": [
+                        {
+                            "name_txt": "child",
+                            "unique_name": "Child <child>",
+                            "name_class": "scientific name",
+                        }
+                    ],
+                    "ranked_lineage": {
+                        "tax_id": 3,
+                        "tax_name": "Archaea",
+                        "species": None,
+                        "genus": None,
+                        "family": None,
+                        "order": None,
+                        "class_": None,
+                        "phylum": None,
+                        "kingdom": None,
+                        "domain": "",
+                    },
                 }
             ],
-            "ranked_lineage": {
-                "tax_id": 3,
-                "tax_name": "Archaea",
-                "species": None,
-                "genus": None,
-                "family": None,
-                "order": None,
-                "class_": None,
-                "phylum": None,
-                "kingdom": None,
-                "domain": ""
-            }
-            }
-        ]
         }
         assert data == expected
+
 
 class TestRankedLineage:
     def test_get_lineage(self, client_with_data: TestClient):
@@ -253,23 +259,23 @@ class TestRankedLineage:
         assert response.status_code == 200
         data = response.json()
         expected = {
-        "count": 1,
-        "offset": 0,
-        "limit": 10,
-        "results": [
-            {
-            "tax_id": 1,
-            "tax_name": "root",
-            "species": None,
-            "genus": None,
-            "family": None,
-            "order": None,
-            "class_": None,
-            "phylum": None,
-            "kingdom": None,
-            "domain": ""
-            }
-        ]
+            "count": 1,
+            "offset": 0,
+            "limit": 10,
+            "results": [
+                {
+                    "tax_id": 1,
+                    "tax_name": "root",
+                    "species": None,
+                    "genus": None,
+                    "family": None,
+                    "order": None,
+                    "class_": None,
+                    "phylum": None,
+                    "kingdom": None,
+                    "domain": "",
+                }
+            ],
         }
         assert data == expected
 
@@ -296,23 +302,22 @@ class TestRankedLineage:
         assert response.status_code == 200
         data = response.json()
         expected = {
-        "count": 3,
-        "offset": 2,
-        "limit": 2,
-        "results": [
-            {
-            "tax_id": 3,
-            "tax_name": "Archaea",
-            "species": None,
-            "genus": None,
-            "family": None,
-            "order": None,
-            "class_": None,
-            "phylum": None,
-            "kingdom": None,
-            "domain": ""
-            }
-        ]
+            "count": 3,
+            "offset": 2,
+            "limit": 2,
+            "results": [
+                {
+                    "tax_id": 3,
+                    "tax_name": "Archaea",
+                    "species": None,
+                    "genus": None,
+                    "family": None,
+                    "order": None,
+                    "class_": None,
+                    "phylum": None,
+                    "kingdom": None,
+                    "domain": "",
+                }
+            ],
         }
         assert data == expected
-
