7921e783dab4bb18692665150f649f3162be8d7b
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2025-12-18 20:46:34 +0100

    generic version method

--- src/biokb_taxtree/__init__.py
+++ src/biokb_taxtree/__init__.py
@@ -1,1 +1,7 @@
-from biokb_taxtree.db.manager import DbManager as Manager
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("biokb_taxtree")
+except PackageNotFoundError:
+    # Package is not installed (e.g., during local development)
+    __version__ = "unknown"
