80ded40b2d36cfd5db3ff786cab8e49413f0cfcc
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2025-12-18 20:47:01 +0100

    standard click file

--- src/biokb_taxtree/cli.py
+++ src/biokb_taxtree/cli.py
@@ -1,26 +1,89 @@
+from importlib.metadata import version
+
 import click
 from sqlalchemy import create_engine
 
+from biokb_taxtree import __version__
 from biokb_taxtree.db.manager import DbManager
+from biokb_taxtree.rdf.neo4j_importer import Neo4jImporter
+from biokb_taxtree.rdf.turtle import TurtleCreator
 
 
-# we are creating a group
 @click.group()
+@click.version_option(
+    version=version("biokb_taxtree"),
+    prog_name="BioKb NCBI Taxonomy Tree",
+)
 def main():
+    """BioKB COCONUT CLI - Manage COCONUT: Import in RDBMS, create turtle files and import into Neo4J.
+
+    Please follow the steps:\n
+    1. Import data using 'import-data' command.\n
+    2. Create TTL files using 'create-ttls' command.\n
+    3. Import TTL files into Neo4j using 'import-neo4j' command.\n
+    """
     pass
 
 
-# advanced method with many different options
-@main.command()
+@main.command("import-data")
+@click.option(
+    "-f",
+    "--force-download",
+    is_flag=True,
+    type=bool,
+    default=False,
+    help="Force re-download of the source file [default: False]",
+)
+@click.option(
+    "-k",
+    "--keep-files",
+    is_flag=True,
+    type=bool,
+    default=False,
+    help="Keep downloaded source files after import [default: False]",
+)
 @click.option(
     "-c",
-    "--sqlalchemy_connection_string",
-    help="SQLAlchemy connection string (dialect+driver://username:password@host:port/database). More info at https://docs.sqlalchemy.org/en/20/core/engines.html",
+    "--connection-string",
+    type=str,
+    default="sqlite:///coconut.db",
+    help="SQLAlchemy engine URL [default: sqlite:///coconut.db]",
 )
-def import_data(
-    sqlalchemy_connection_string: str | None = None,
-):
-    engine = None
-    if sqlalchemy_connection_string:
-        engine = create_engine(sqlalchemy_connection_string)
-    DbManager(engine=engine).import_data()
+def import_data(force_download: bool, connection_string: str, keep_files: bool):
+    """Import COCONUT data."""
+    engine = create_engine(connection_string)
+    DbManager(engine=engine).import_data(
+        force_download=force_download, keep_files=keep_files
+    )
+    click.echo(f"Data imported successfully to {connection_string}")
+
+
+# TODO: allow specifying start tax ids
+@main.command("create-ttls")
+@click.option(
+    "-c",
+    "--connection-string",
+    type=str,
+    default="sqlite:///coconut.db",
+    help="SQLAlchemy engine URL [default: sqlite:///coconut.db]",
+)
+def create_ttls(connection_string: str):
+    """Create TTL files from local database."""
+    path_to_zip = TurtleCreator(create_engine(connection_string)).create_ttls()
+    click.echo(
+        f"Path to the zip file containing all generated Turtle files. {path_to_zip}"
+    )
+
+
+# TODO: path to zip file or files folder
+@main.command("import-neo4j")
+@click.option("--uri", "-i", default="bolt://localhost:7687", help="Neo4j database URI")
+@click.option("--user", "-u", default="neo4j", help="Neo4j username")
+@click.option("--password", "-p", required=True, help="Neo4j password")
+def import_neo4j(uri: str, user: str, password: str):
+    """Import TTL files into Neo4j database."""
+    Neo4jImporter(neo4j_uri=uri, neo4j_user=user, neo4j_pwd=password).import_ttls()
+
+
+if __name__ == "__main__":
+    main()
